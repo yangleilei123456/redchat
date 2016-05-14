c613b3b727bc2ba7a2f1e909d17da960b2131e24
yangleilei123456 <508847654@qq.com>
2016-05-14 15:21:03 +0800

    flush -5min

--- redchatroom/client.c
+++ redchatroom/client.c
@@ -1,12 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<time.h>
 #include"cJSON.h"
+typedef enum {true=1,false=0}bool;
 void print()
 {
 	printf("		***********************\n");
 	printf("	1:register()\n");
 	printf("	2:login()\n");
 	printf("		***********************\n");
+}
+int socket_bind()
+{
+	int sockfd;
+	struct sockaddr_in ser;
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	bzero(&ser,sizeof(ser));
+	ser.sin_family=AF_INET;
+	ser.sin_port=htons(6000);
+	inet_pton(AF_INET,"127.0.0.1",&ser.sin_addr);
+	connect(sockfd,(struct sockaddr*)&ser,sizeof(ser));
+	return sockfd;
+}
+void registe()
+{
+	int sockfd = socket_bind();
+
+}
+void login()
+{
+
 }
 bool choose()
 {
@@ -16,10 +43,11 @@ bool choose()
 	scanf("%d",&flag);
 	switch(flag)
 	{
-		case 1:register();break;
+		case 1:registe();break;
 		case 2:login();break;
 		default:printf("no choose\n"),res=false;break;
 	}
+	return res;
 }
 int main()
 {
